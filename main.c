c22924b7de59766e067079cd20e0112813c97d76
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2024-03-07 19:18:46 +0900

    can render a box

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+// macro for writing errors to terminal
+int scc(int code)
+{
+    if (code < 0)
+    {
+        fprintf(stderr, "SDL Error :%s\n", SDL_GetError());
+        exit(1);
+    }
+    return code;
+}
+
+void *scp(void *ptr)
+{
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "SDL Error :%s\n", SDL_GetError());
+        exit(1);
+    }
+    return ptr;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World.!");
+    // SDL can fail so we have to do a check here
+    scc(SDL_Init(SDL_INIT_VIDEO));
+
+    SDL_Window *const window = scp(SDL_CreateWindow("Hunger games", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE));
+
+    if (window == NULL)
+    {
+        printf("SDL Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Renderer *const renderer = scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+    int quit = 0;
+    while (!quit)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                quit = 1;
+                break;
+
+            default:
+                break;
+            }
+        }
+        scc(SDL_RenderClear(renderer));
+    }
+
+    SDL_Quit();
     return 0;
 }
